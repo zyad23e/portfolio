1e4ad0550bfe07c9744140d16545660bfb4bf90a
zyad23e <zyade23@arizona.edu>
2026-02-10 16:39:49 -0700

    skills updated

--- src/components/About/TechList/index.js
+++ src/components/About/TechList/index.js
@@ -4,12 +4,12 @@ import "./index.scss"
 
 const TechList = () => {
   const [techs] = useState([
-    {name: "Spring Security ", level: "Proficient", progress: 80, className: "security-progress"},
+    {name: "Spring Security ", level: "Proficient", progress: 90, className: "security-progress"},
+    {name: "React ", level: "Proficient", progress: 80, className: "jdbc-progress"},
     {name: "Spring Data JPA/Hibernate ", level: "Proficient", progress: 80, className: "jpa-progress"},
     {name: "REST APIs ", level: "Proficient", progress: 75, className: "rest-progress"},
     {name: "Maven ", level: "Proficient", progress: 70, className: "maven-progress"},
-    {name: "JDBC ", level: "Familiar", progress: 55, className: "jdbc-progress"},
-    {name: "Thymeleaf ", level: "Familiar", progress: 55, className: "thymeleaf-progress"}
+    {name: "Thymeleaf ", level: "Proficient", progress: 70, className: "thymeleaf-progress"}
   ]);
 
   return (
